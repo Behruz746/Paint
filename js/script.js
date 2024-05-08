c6e23eb57d1736ca10ac89e7fcb5b30f26e6a85b
Behruz <madaminovbehruz746@gmail.com>
2024-05-08 11:20:06 +0500

    finished: full project

--- js/script.js
+++ js/script.js
@@ -2,22 +2,57 @@
 const canvesEl = document.querySelector("#point-board"),
   sizeSliderEl = document.querySelector("#size-slider"),
   toolBtns = document.querySelectorAll(".tool"),
-  fillColorEl = document.querySelector("#fill-color");
+  fillColorEl = document.querySelector("#fill-color"),
+  colorBtns = document.querySelectorAll(".colors .option"),
+  colorPicker = document.querySelector("#color-picker"),
+  clearBtn = document.querySelector(".clear-canvas"),
+  saveImgBtn = document.querySelector(".save-img");
 
 // Canves uchun saqlovchilar
 let ctx = canvesEl.getContext("2d"),
   isDrowing = false,
   brushWidth = 5,
   selectedTool = "brush",
+  selectedColor = "#000000",
   prevMouseX,
   prevMouseY,
   snapshot;
+
+// set canvas background color
+const setCanvasBackound = () => {
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(0, 0, canvesEl.width, canvesEl.height);
+  // ctx.fillStyle = selectColor;
+};
 
 // window yuklandanidan song canvesga width va height olchamlarini berish
 window.addEventListener("load", () => {
   canvesEl.width = canvesEl.offsetWidth;
   canvesEl.height = canvesEl.offsetHeight;
+  setCanvasBackound();
 });
+
+// chizish uchun colorlarni olish
+const selectColor = (btn) => {
+  let myDivObjBgColor = window.getComputedStyle(btn).backgroundColor;
+  document.querySelector(".options .selected").classList.remove("selected");
+  btn.classList.add("selected");
+  selectedColor = myDivObjBgColor;
+};
+
+// chizish uchun colorlarni olish input orqali
+const selectColorInput = (e) => {
+  console.log(colorPicker.value);
+
+  if (colorPicker.value === "#ffffff") {
+    colorPicker.parentElement.style.border = "1px solid #bfbfbf";
+  } else {
+    colorPicker.parentElement.style.backgroundColor = colorPicker.value;
+    colorPicker.parentElement.style.border = "0";
+  }
+
+  selectedColor = e.target.value;
+};
 
 // mouse down bolgani dan song isDrowing true qiladogan function
 const startDrow = (e) => {
@@ -30,6 +65,8 @@ const startDrow = (e) => {
   // chiziq qalinligi
   ctx.lineWidth = brushWidth;
   snapshot = ctx.getImageData(0, 0, canvesEl.width, canvesEl.height);
+  ctx.strokeStyle = selectedColor;
+  ctx.fillStyle = selectedColor;
   console.log(snapshot);
 };
 
@@ -50,7 +87,7 @@ const drowRectangle = (e) => {
     );
   } else {
     // agar fillColorEl true bolsa unda fill color bo'lsin fillRect()
-    ctx.fillStyle = "red";
+    // ctx.fillStyle = "red";
     ctx.fillRect(
       e.offsetX,
       e.offsetY,
@@ -67,11 +104,9 @@ const drowCircle = (e) => {
     Math.pow(prevMouseX - e.offsetX, 2) + Math.pow(prevMouseY - e.offsetY, 2)
   );
   ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
-
   if (!fillColorEl.checked) {
     ctx.stroke();
   } else {
-    ctx.fillStyle = "red";
     ctx.fill();
   }
 };
@@ -87,7 +122,6 @@ const drowTriangle = (e) => {
   if (!fillColorEl.checked) {
     ctx.stroke();
   } else {
-    ctx.fillStyle = "red";
     ctx.fill();
   }
 };
@@ -121,15 +155,20 @@ const drowing = (e) => {
       drowTriangle(e);
       break;
 
+    // uchurg'ich
+    case "eraser":
+      ctx.strokeStyle = "#ffffff";
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+      break;
+
     default:
       break;
   }
 
-  // chiziqni rangi
-  ctx.strokeStyle = "red";
   // parametorlar
   console.log(
-    `Mouse-X: ${e.offsetX}px,\nMouse-Y: ${e.offsetY}px,\nLine-width: ${brushWidth}px,\nPoint-tool: ${selectedTool},\nFill-color: ${fillColorEl.checked}`
+    `Mouse-X: ${e.offsetX}px,\nMouse-Y: ${e.offsetY}px,\nLine-width: ${brushWidth}px,\nPoint-tool: ${selectedTool},\nFill-color: ${fillColorEl.checked}\nStroke-style: ${selectedColor},\nFill-style: ${selectedColor}`
   );
 };
 
@@ -141,11 +180,34 @@ const activeToolBtns = (btn) => {
   selectedTool = btn.id;
 };
 
+const saveImage = () => {
+  let canvasUrl = canvesEl.toDataURL("image/jpeg", 1);
+  saveImgBtn.href = canvasUrl;
+  if (window.navigator.msSaveBlod) {
+    window.navigator.msSaveBlod(canvesEl.msSaveBlod(), "canvas-image.png");
+  }
+};
+
+saveImgBtn.addEventListener("click", saveImage);
+
+// chiziqlarni hammasini ochirish
+const clearCanves = () => {
+  ctx.clearRect(0, 0, canvesEl.width, canvesEl.height);
+  setCanvasBackound();
+};
+
 toolBtns.forEach((btn) =>
   btn.addEventListener("click", () => activeToolBtns(btn))
 );
+
 // chiziqni width setting qilish
 sizeSliderEl.addEventListener("change", (e) => (brushWidth = +e.target.value));
+colorBtns.forEach((btn) =>
+  btn.addEventListener("click", () => selectColor(btn))
+);
+colorPicker.addEventListener("input", selectColorInput);
 canvesEl.addEventListener("mousedown", startDrow);
 canvesEl.addEventListener("mouseup", stopDrow);
+// canvesEl.addEventListener("mouseout", stopDrow);
 canvesEl.addEventListener("mousemove", drowing);
+clearBtn.addEventListener("click", clearCanves);
